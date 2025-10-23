ff8fa3aead317c1bad4df939682d1bd89d54b8fd
eschlatter <eschlatter@users.noreply.github.com>
2025-10-23 15:03:12 -0500

    Speed up IBM

--- functions/f_RunIBM.r
+++ functions/f_RunIBM.r
@@ -14,38 +14,46 @@ f_RunIBM <- function(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,
   
   ########## Data structure to describe population ##########
   start_sites=rep(patch_locations$id,patch_locations$K_i)
-
-  pop <- data.frame(t=1,
-                    origin_site=NA, 
-                    alpha=alpha_start, # note that these are the INDICES of the parameters in v_alphas and v_thetas, not the actual parameter values
-                    theta=theta_start,
-                    p=p_start,
-                    dest_site=start_sites) # start at carrying capacity everywhere
+  
+  pop <- vector("list",length=nsteps) # list. List element t is the adults at timestep t.
+  
+  # initialize first generation of adults
+  adults <- data.frame(t=1,
+                       origin_site=NA, 
+                       alpha=alpha_start, # note that these are the INDICES of the parameters in v_alphas and v_thetas, not the actual parameter values
+                       theta=theta_start,
+                       p=p_start,
+                       dest_site=start_sites) # start at carrying capacity everywhere
+  pop[[1]] <- adults
   
   ########## Simulation ##########
   for(t_step in 1:nsteps){
-    adults <- filter(pop,t==t_step)
-    
     # reproduction
+    # create a dataframe with the same columns as adults and sum(K*b) number of rows, all empty
     larvae <- adults[FALSE,]
+    larvae[1:sum(K*b),] <- NA
+    
+    on_row_larvae <- 1 #first row of larvae available for filling
     for(i_adult in 1:nrow(adults)){
       b_i <- patch_locations$b_i[adults$dest_site[i_adult]] # get the birth rate in that adult's patch
-      larvae <- rbind(larvae,bind_rows(replicate(b_i, adults[i_adult,], simplify = FALSE))) 
+      larvae[on_row_larvae:(on_row_larvae+b_i-1),] <- adults[i_adult,]
+      on_row_larvae <- on_row_larvae+b_i
     }
     larvae <- mutate(larvae, origin_site=dest_site) %>%
       mutate(dest_site=NA) %>%
-      mutate(t=t_step+1)
+      mutate(t=t_step+1) ### if we were going to add b_i to the dataframe, this would be the place to do it
     
     # dispersal
     for(i in 1:nrow(larvae)){
-      b_i <- patch_locations$b_i[larvae$origin_site[i]] # habitat quality (b) in larva's origin patch. Maybe at this point it makes sense to just add this as a column.
-      i_alpha <- larvae$alpha[i] # current larva's alpha index
-      alpha_plastic <- case_when( # current larva's effective alpha index (given plasticity)
-        b_i==b_neutral ~ i_alpha,
-        b_i==b_bad ~ oob_squish(i_alpha+round(larvae$p[i]),c(1,length(v_alphas))), # we'll want something more sophisticated than round(v_p[i_p]) eventually
-        b_i==b_good ~ oob_squish(i_alpha-round(larvae$p[i]),c(1,length(v_alphas)))
-      )
-      dests <- conn_matrices[alpha_plastic, larvae[i,]$theta, larvae[i,]$origin_site, ]
+      # b_i <- patch_locations$b_i[larvae$origin_site[i]] # habitat quality (b) in larva's origin patch. Maybe at this point it makes sense to just add this as a column.
+      # i_alpha <- larvae$alpha[i] # current larva's alpha index
+      # alpha_plastic <- case_when( # current larva's effective alpha index (given plasticity)
+      #   b_i==b_neutral ~ i_alpha,
+      #   b_i==b_bad ~ oob_squish(i_alpha+round(larvae$p[i]),c(1,length(v_alphas))), # we'll want something more sophisticated than round(v_p[i_p]) eventually
+      #   b_i==b_good ~ oob_squish(i_alpha-round(larvae$p[i]),c(1,length(v_alphas)))
+      # )
+      # dests <- conn_matrices[alpha_plastic, larvae[i,]$theta, larvae[i,]$origin_site, ]
+      dests <- conn_matrices[larvae$alpha[i], larvae$theta[i], larvae$origin_site[i], ]
       larvae[i,]$dest_site <- sample(1:npatch,size=1,prob=dests)
     } # i
     
@@ -60,14 +68,18 @@ f_RunIBM <- function(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,
     # (if there are fewer than K larvae at a site, they'll all be kept)
     # larvae <- group_by(larvae,dest_site) %>%
     #   slice_sample(n=K)
+    adults <- larvae[FALSE,]
+    adults[1:sum(K),] <- NA
+    on_row_survivors <- 1
     for(site in unique(larvae$dest_site)){
-      survivors <- filter(larvae,dest_site==site) %>%
+      site_survivors <- filter(larvae,dest_site==site) %>%
         slice_sample(n=filter(patch_locations,id==site)$K_i)
-      pop <- rbind(pop,survivors)
+      adults[on_row_survivors:(on_row_survivors+nrow(site_survivors)-1),] <- site_survivors
+      on_row_survivors <- on_row_survivors+nrow(site_survivors)
     }
     
     # cleanup for next timestep
-    # pop <- rbind(pop,larvae)
+    pop[[t_step]] <- adults
     
     if(t_step %% round(nsteps/10) == 0) print(t_step)
     
@@ -78,13 +90,16 @@ f_RunIBM <- function(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,
     
   } # t_step
   
+  # combine timesteps into one dataframe
+  pop_df <- do.call('rbind',pop)
+  
   # add values (not just indices) of parameters to dataframe
-  pop <- mutate(pop,alpha_value=v_alphas[alpha],theta_value=v_thetas[theta])
+  pop_df <- mutate(pop_df,alpha_value=v_alphas[alpha],theta_value=v_thetas[theta])
   
   ########## Process data for plotting ##########
-  by_t <- summarize(group_by(pop,t),alpha=mean(alpha_value),theta=mean(theta_value),popsize=n())
+  by_t <- summarize(group_by(pop_df,t),alpha=mean(alpha_value),theta=mean(theta_value),popsize=n())
   
   ########## Output ##########
   time_run <- proc.time()-starttime
-  return(list(pop,by_t, patch_locations, time_run))
+  return(list(pop_df,by_t, patch_locations, time_run))
 }

377eb94a611ec364f400143820ffc6b6e3fb7e81
eschlatter <eschlatter@users.noreply.github.com>
2025-11-24 11:45:55 -0600

    Update setup and Run_Sims files

--- 1_Generate_Param_Sets.R
+++ 1_Generate_Param_Sets.R
@@ -1,8 +1,8 @@
 source('0_Setup.R')
 
 ########## Param set 1: 
-nx <- 10 # size of space in the x dimension
-ny <- 10 # size of space in the y dimension
+nx <- 20 # size of space in the x dimension
+ny <- 20 # size of space in the y dimension
 nsteps <- 10 # timesteps
 # dispersal kernel is a gamma distribution, shape=alpha, scale=theta
 v_alphas <- seq(from=0.01,to=5,length.out=5) # values the shape parameter can take
@@ -12,12 +12,12 @@ theta_start <- 3 # index in (v_thetas) of scale parameter initial value
 v_p <- seq(from=0,to=0.9,length.out=2) # values the plasticity parameter can take
 p_start <- 1 # index (in v_p) of plasticity parameter initial value
 mu <- 0.01 # mutation frequency
-b <- 10 # reproductive output
+b <- 8 # reproductive output
 K <- round(runif(n=nx*ny,min=3,max=15)) # carrying capacity (also what plasticity responds to)
 #pars_save <- list(nx=nx,ny=ny,nsteps=nsteps,v_alphas=v_alphas,alpha_start=alpha_start,v_thetas=v_thetas,theta_start=theta_start,v_p=v_p,p_start=p_start,mu=mu,b=b,b_bad=b_bad,b_neutral=b_neutral,b_good=b_good,K=K)
 #save(pars_save,file='params/ParSet1.RData')
 
-patch_locations=NULL
+#patch_locations=NULL
 b_neutral=b
 b_bad=4
 b_good=16
--- 3_Run_Sims.R
+++ 3_Run_Sims.R
@@ -1,31 +1,60 @@
 source('0_Setup.R')
 
-# load parameters
-load('params/ParSet1.RData')
-list2env(pars_save,envir=environment())
+# # load parameters
+# load('params/ParSet1.RData')
+# list2env(pars_save,envir=environment())
 
 # #simulate seascape (use real base map)
 # load('seascapes/bze_map_sub.RData')
 # bze_out <- f_GenerateMapWithK(base_map, K_range=c(20,35), h = 0.6, plot_flag=TRUE)
 # list2env(bze_out,envir=environment())
 # 
-# # or simulate seascapes (use fractal base map)
-# frac_out <- f_GenerateMapWithK(K_range=c(1,15), h=1.8, k=1, p=0.3, h_base=0.8, plot_flag=TRUE)
-# list2env(frac_out,envir=environment())
+# or simulate seascapes (use fractal base map)
+frac_out <- f_GenerateMapWithK(K_range=c(1,15), h=1.8, k=5, p=0.5, h_base=0.8, plot_flag=TRUE)
+list2env(frac_out,envir=environment())
 
 ## run the model
-nsteps=300
+source('1_Generate_Param_Sets.R')
 patch_locations=NULL
-sim_out <- f_RunMatrix(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,b_bad,b_neutral,b_good,K,patch_locations = patch_locations, disturb_prob=0.1)
-ranjan_sim <- f_RunMatrixLoop(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL)
-ibm_sim <- f_RunIBM(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,b_bad,b_neutral,b_good,K,heatmap_plot_int=5000)
+nsteps=1000
+sim_sparse <- f_RunMatrixSparse(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,b_bad,b_neutral,b_good,K,patch_locations = patch_locations, seed=NULL)
+sim_loop <- f_RunMatrixLoop(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0.1,patch_locations=NULL, seed=NULL)
+sim_loop_new <- f_RunMatrixLoopNew(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL, seed=40,file_out="model_runs/20251121_1.h5")
+#sim_ibm <- f_RunIBM(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,b_bad,b_neutral,b_good,K,heatmap_plot_int=5000)
+
+sim_loop1 <- f_RunMatrixLoop(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL, seed=40)
+sim_loop2 <- f_RunMatrixLoop2(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL, seed=40)
+
+# check any changes didn't affect output
+all.equal(sim_loop1$by_t,sim_loop2$by_t)
+
+# timing
+sim_sparse$time_run
+sim_loop$time_run
+sim_loop2$time_run
+
 
 ## plot output
-f_PlotOutput(sim_out$by_t, kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
-f_PlotAllHeatmaps(sim_out$sim_melt,sim_out$patch_locations)
+f_PlotOutput(sim_sparse$by_t, kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
+f_PlotAllHeatmaps(sim_sparse$sim_melt,sim_sparse$patch_locations)
 
-f_PlotOutput(ranjan_sim$by_t, kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
-f_PlotAllHeatmaps(ranjan_sim$sim_melt,ranjan_sim$patch_locations)
+f_PlotOutput(sim_loop$by_t, kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
+f_PlotAllHeatmaps(sim_loop$sim_melt,sim_loop$patch_locations,plot_int=seq(from=489,to=500,by=1))
 
-f_PlotOutput(ibm_sim[[2]], kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
+f_PlotOutput(sim_loop_new$by_t, kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
+f_PlotAllHeatmaps(sim_loop_new$sim_melt,sim_loop_new$patch_locations)
 
+#f_PlotOutput(ibm_sim[[2]], kern_timesteps=seq(from=0.75*nsteps,to=nsteps,length.out=25))
+
+## time profile
+Rprof(filename='profile_loop.out')
+sim_loop <- f_RunMatrixLoop(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL, seed=NULL)
+Rprof(NULL)
+summaryRprof('profile_loop.out')
+
+
+## memory profile
+Rprofmem(filename='profile_loop.out',threshold=10000)
+sim_loop <- f_RunMatrixLoop(nx,ny,nsteps,v_alphas,v_thetas,v_p,alpha_start,theta_start,p_start,mu,b,K,disturb_prob=0,patch_locations=NULL, seed=NULL)
+Rprofmem(NULL)
+noquote(readLines("profile_loop.out"))
